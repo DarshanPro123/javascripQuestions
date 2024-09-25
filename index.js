7c719be27e475495d5924bf656cff30cbef3b108
Darshanpro123 <darshanpro187@gmail.com>
2024-09-25 17:33:18 +0530

    add functions Date 25-Sep-2024

--- index.js
+++ index.js
@@ -1,84 +1,60 @@
 console.log("Grp");
 
 // 1. Defined array with 6 elements and add an element at 1st, 4th and last position.
-
-const arr = [1, "darshan", 2, 4, 4, 6];
-
-const newarr = [...arr]; // copy of array
-
-newarr.unshift(3); //att first position
-
-console.log(...newarr);
-
-// it's splice using for 3 index to (3,1 it's stand for del that index value of current , add value or number)
-newarr.splice(3, 2, "ds"); //at fourth position
-
-console.log(newarr);
-
-newarr.push("Lss");
-console.log(newarr);
-
-const data = arr.map((item, index) => {
-  if (index === 0) {
-    return "Firstele";
-  } else if (index === 3) {
-    return "fourth";
-  } else if (index === 5) {
-    return "last position";
-  }
-
-  return item;
-});
-
-console.log(data);
-
 // 3. Defined array with 6 elements and remove an element from 4th position using any 2 solutions.
-
-const arr1 = [1, 5, 334, 57, 5467, 346];
-const demo1 = [...arr1];
-demo1.splice(3, 1); //splice 4th position array
-console.log(demo1);
-
-const indx = 3;
-
-const newarr1 = [...arr1.slice(0, indx), ...arr1.slice(indx + 1)];
-
-//it's make with spread to arr1  first one is slice 0 index thhi 3 index pela na element and
-//2nd wala ma index pachi na element spread thay che
-
-console.log(newarr1);
-
 // 4. Define 2 arrays and merge both arrays using any 2 solutions.
+// 5. Create array of objects with properties firstname, lastname and age and add 1 property(job) in every object.
+// 6. Create array of objects with properties firstname, lastname and age and remove item if item’s age is 40.
+
+function addElement() {
+  const addArr = [1, "darshan", 2, 4, 4, 6];
+  // copy of array
+  const updatedArr = [...addArr];
+  //att first position
+  updatedArr.unshift(3);
+  //at fourth position
+  updatedArr.splice(3, 2, "ds");
+  // it's splice using for 3 index to (3,1 it's stand for del that index value of current , add value or number)
+  //at last position
+  updatedArr.push("Lss");
+  return updatedArr;
+}
+
+//first solution ---->
+function deletefrthEle() {
+  const num = [1, 5, 334, 57, 5467, 346];
+  num.splice(3, 1); //splice 4th position array
+  return num;
+}
+
+//secound solution---->
+
+function delEleByslice() {
+  const sol2 = [1, 5, 334, 57, 5467, 346];
+  const indx = 3;
+  const deleteEle = [...sol2.slice(0, indx), ...sol2.slice(indx + 1)];
+  return deleteEle;
+}
+
 // 1st solution--------->
 
-const dem1 = [1, 2, 4, 5, 6, 7];
-
-const demo2 = ["d", "a", "r", "s", "h", "a", "n"];
-
-const mixed = dem1.concat(demo2);
-console.log(mixed);
+function mergingArrConc() {
+  const dem1 = [1, 2, 4, 5, 6, 7];
+  const demo2 = ["d", "a", "r", "s", "h", "a", "n"];
+  const mixed = dem1.concat(demo2);
+  return mixed;
+}
 
 //2nd solution---------->
 
-const mixed2 = [...dem1, ...demo2];
-console.log(mixed2);
+function addArrayBySpred() {
+  const demA = [1, 2, 4, 5, 6, 7];
+  const demoB = ["d", "a", "r", "s", "h", "a", "n"];
+  const mixed2 = [...demA, ...demoB];
+  return mixed2;
+}
 
-// 5. Create array of objects with properties firstname, lastname and age and add 1 property(job) in every object.
-
-const myinfo = [
-  {
-    frstname: "Panchal",
-    lastname: "darshan",
-    age: 20,
-  },
-];
-
-const myinfoadd = { ...myinfo, job: "React" };
-console.log(myinfoadd);
-
-// 6. Create array of objects with properties firstname, lastname and age and remove item if item’s age is 40.
-
-const allprofiles = [
+const myprofile = [
   {
     frstname: "Panchal",
     lastname: "darshan",
@@ -101,11 +77,42 @@ const allprofiles = [
   },
 ];
 
-const { age } = allprofiles;
+function addPropertie() {
+  const mydatta = myprofile.map((profile) => {
+    return { ...profile, job: "dev" };
+  });
+  return mydatta;
+}
 
-const newProfile = [
-  ...allprofiles,
-  allprofiles.filter((profile) => profile.age !== 40),
-];
+const allprofiles = [...myprofile];
 
-console.log(...newProfile);
+function youngPeopleshow() {
+  const newProfile = allprofiles.filter((profile) => profile.age !== 40);
+  return newProfile;
+}
+
+//1.
+
+console.log(addElement());
+
+//2
+
+//2.1-->
+console.log(deletefrthEle());
+
+//2.2-->
+console.log(delEleByslice());
+
+//3
+
+//3.1-->
+console.log(mergingArrConc());
+
+//3.2-->
+console.log(addArrayBySpred());
+
+//4
+console.log(addPropertie());
+
+//5
+console.log(youngPeopleshow());

3561b61bbab8b5a45ec139ce5a2fd85f0071cdaf
Darshanpro123 <darshanpro187@gmail.com>
2024-10-07 13:59:40 +0530

    Edit date in 1st question answer

--- 5thTask_07_10.js
+++ 5thTask_07_10.js
@@ -4,30 +4,30 @@ console.clear();
 // 2. Write a function to find the largest numbers that can be formed by swapping two numbers of a given number.
 // 3. Write a function to calculate the nth number in the Tribonacci sequence.
 
-const firstDate = new Date("2020-09-01");
+const firstDate = new Date("2024-10-07");
 
-const lastDate = new Date("2020-09-12");
+const lastDate = new Date("2025-01-14");
 
 const calculateDays = (firstDate, lastDate) => {
   const oneDay = 24 * 60 * 60 * 1000; // hours*minutes*seconds*milliseconds
-  const diffDays = Math.round(Math.abs((firstDate - lastDate) / oneDay));
-  return diffDays;
+  const differentDays = Math.abs((firstDate - lastDate) / oneDay);
+  return differentDays;
 };
 
 console.log(calculateDays(firstDate, lastDate));
 
 //2. Write a function to find the largest numbers that can be formed by swapping two numbers of a given number.
 
-const swapNumbers = (num) => {
-  const numStr = num.toString().split("");
-  const maxNum = numStr
+const getMaximumValue = (num) => {
+  const stringValue = num.toString().split("");
+  const maximumValue = stringValue
     .slice()
     .sort((a, b) => b - a)
     .join("");
-  return maxNum;
+  return maximumValue;
 };
 
-console.log(swapNumbers(2342));
+console.log(getMaximumValue(2342));
 
 //3. Write a function to calculate the nth number in the Tribonacci  sequence.
 

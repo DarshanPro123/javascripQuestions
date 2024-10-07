4dcb2f9c0d7b338adad8afce474fa0ab49fe7115
Darshanpro123 <darshanpro187@gmail.com>
2024-10-07 13:52:33 +0530

    Edit 5thTask file and complate task

--- 5thTask_07_10.js
+++ 5thTask_07_10.js
@@ -1,3 +1,5 @@
+console.clear();
+
 // 1. Calculate the Number of Days Between Two Dates
 // 2. Write a function to find the largest numbers that can be formed by swapping two numbers of a given number.
 // 3. Write a function to calculate the nth number in the Tribonacci sequence.
@@ -22,13 +24,19 @@ const swapNumbers = (num) => {
     .slice()
     .sort((a, b) => b - a)
     .join("");
-  const minNum = numStr
-    .slice()
-    .sort((a, b) => a - b)
-    .join("");
-  return [maxNum, minNum];
+  return maxNum;
 };
 
-console.log(swapNumbers(3734));
+console.log(swapNumbers(2342));
 
-//3.
+//3. Write a function to calculate the nth number in the Tribonacci  sequence.
+
+const getTribonacci = (n) => {
+  const tribonacci = [0, 0, 1];
+  for (let i = 3; i <= n; i++) {
+    tribonacci[i] = tribonacci[i - 1] + tribonacci[i - 2] + tribonacci[i - 3];
+  }
+  return tribonacci[n];
+};
+
+console.log(getTribonacci(12));
